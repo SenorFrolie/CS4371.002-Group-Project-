f83a052c194570dc78fc54c3e4bf17a8be8cc782
dallasgere <dallas.gere05@gmail.com>
2022-10-20 20:05:38 -0500

    the server is running now

---
+++ server.py
@@ -0,0 +1,9 @@
+import flask
+
+app = flask.Flask(__name__)
+@app.route('/')
+def front_page():
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
