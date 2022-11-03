cbe47d5814c8ffb4e4dadd12075a0db29addd8f8
dallasgere <dallas.gere05@gmail.com>
2022-11-03 10:14:39 -0500

    trying some javascript and drop down menus

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import flask
 
 app = flask.Flask(__name__)
+
 @app.route('/')
 def front_page():
     return flask.render_template('front_page.html')
+
+@app.route('/suggest', methods=['POST'])
+def suggest():
+    return flask.render_template('suggest.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
