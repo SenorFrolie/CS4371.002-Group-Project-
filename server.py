dfd8c70b76a781882952431598dfb1fd75bfc3cd
dallasgere <dallas.gere05@gmail.com>
2022-11-03 11:49:32 -0500

    changing the file names again and adding a javascript file for the suggestion form page

--- server.py
+++ server.py
@@ -4,11 +4,17 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def front_page():
+    '''
+    this is the very first page that the user will see when the visit our site
+    '''
     return flask.render_template('front_page.html')
 
-@app.route('/suggest', methods=['POST'])
-def suggest():
-    return flask.render_template('suggest.html')
+@app.route('/suggest_form', methods=['POST', 'GET'])
+def suggest_form():
+    '''
+    this is the page where we will ask users their suggestions and retrieve their answers
+    '''
+    return flask.render_template('suggest_form.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
