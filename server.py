8ae7f287b1bf11a8fd20f1cfa6d6dc1034a395ea
dallasgere <dallas.gere05@gmail.com>
2022-11-03 09:57:59 -0500

    changing file names to make things easier to understand

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import flask
 app = flask.Flask(__name__)
 @app.route('/')
 def front_page():
-    return flask.render_template('index.html')
+    return flask.render_template('front_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
