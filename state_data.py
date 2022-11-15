e0abd56cd50a30431b01d30d67c996e5d86ca8c0
dallasgere <dallas.gere05@gmail.com>
2022-11-15 12:01:25 -0600

    adding a dashboard route in the server file, added dashboard.html in templates, added dashboard.css to static

--- state_data.py
+++ state_data.py
@@ -2,22 +2,13 @@
 this is the file that has all our city data in it
 """
 
+
 class StateData:
-    '''
+    """
     this is the class that will contain all our data form our states
-    '''
-    
+    """
+
     states = [
-        {
-            'name': 'alabama',
-            'snow': 'no',
-            'rain': 'no',
-            'ocean': 'no'
-        },
-        {
-            'name': 'arkansas',
-            'snow': 'yes',
-            'rain': 'no',
-            'ocean': 'no'
-        }
+        {"name": "alabama", "snow": "no", "rain": "no", "ocean": "no"},
+        {"name": "arkansas", "snow": "yes", "rain": "no", "ocean": "no"},
     ]
