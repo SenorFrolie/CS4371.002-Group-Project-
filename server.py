4d1cc5a0628f03783f72a7eef6b0c9d05338191e
dallasgere <dallas.gere05@gmail.com>
2022-11-16 14:51:11 -0600

    points array working, now i just need to choose the top ones

--- server.py
+++ server.py
@@ -62,7 +62,7 @@ def suggest_page():
     tax = drop_down_form["tax_answer"]
     rain = drop_down_form["rain_answer"]
     coast = drop_down_form["coast_answer"]
-    tax = drop_down_form["tax_answer"]
+    income = drop_down_form["income_answer"]
     political = drop_down_form["political_answer"]
     university = drop_down_form["uni_answer"]
     mountain = drop_down_form["mountain_answer"]
@@ -72,29 +72,103 @@ def suggest_page():
     home = drop_down_form["home_answer"]
     crime = drop_down_form["crime_answer"]
 
+    # declaring points array and initializing
     points = []
     for i in range(50):
-        points[i] = 0
+        points.append(0)
 
     state_obj = StateData
     count = 0
-    for i in state_obj.states:
-        if snow == 'yes':
-            if state_obj.states[count]['snow_anser'] == 'yes':
+    #for i in state_obj.states:
+    for count, i in enumerate(state_obj.states):
+        if snow == "yes":
+            if state_obj.states[count]["snow"] == "yes":
                 points[count] += 1
-        elif snow == 'no':
-            if state_obj.states[count]['snow_anser'] == 'no':
+        elif snow == "no":
+            if state_obj.states[count]["snow"] == "no":
                 points[count] += 1
-        if rural == 'yes':
-            if state_obj.states[count]['rural_anser'] == 'yes':
+        if rural == "yes":
+            if state_obj.states[count]["rural_urban"] == "yes":
                 points[count] += 1
-        elif rural == 'no':
-            if state_obj.states[count]['rural_anser'] == 'no':
+        elif rural == "no":
+            if state_obj.states[count]["rural_urban"] == "no":
                 points[count] += 1
-    
+        if tax == "yes":
+            if state_obj.states[count]["income_tax"] == "yes":
+                points[count] += 1
+        elif tax == "no":
+            if state_obj.states[count]["income_tax"] == "no":
+                points[count] += 1
+        if rain == "yes":
+            if state_obj.states[count]["rain"] == "yes":
+                points[count] += 1
+        elif rain == "no":
+            if state_obj.states[count]["rain"] == "no":
+                points[count] += 1
+        if coast == "yes":
+            if state_obj.states[count]["coastline"] == "yes":
+                points[count] += 1
+        elif coast == "no":
+            if state_obj.states[count]["coastline"] == "no":
+                points[count] += 1
+        if income == "yes":
+            if state_obj.states[count]["avg_income"] == "yes":
+                points[count] += 1
+        elif income == "no":
+            if state_obj.states[count]["avg_income"] == "no":
+                points[count] += 1
+        if political == "yes":
+            if state_obj.states[count]["political_climate"] == "yes":
+                points[count] += 1
+        elif political == "no":
+            if state_obj.states[count]["political_climate"] == "no":
+                points[count] += 1
+        if university == "yes":
+            if state_obj.states[count]["university"] == "yes":
+                points[count] += 1
+        elif university == "no":
+            if state_obj.states[count]["university"] == "no":
+                points[count] += 1
+        if mountain == "yes":
+            if state_obj.states[count]["mountains"] == "yes":
+                points[count] += 1
+        elif mountain == "no":
+            if state_obj.states[count]["mountians"] == "no":
+                points[count] += 1
+        if desert == "yes":
+            if state_obj.states[count]["desert"] == "yes":
+                points[count] += 1
+        elif desert == "no":
+            if state_obj.states[count]["desert"] == "no":
+                points[count] += 1
+        if population == "yes":
+            if state_obj.states[count]["population"] == "yes":
+                points[count] += 1
+        elif population== "no":
+            if state_obj.states[count]["population"] == "no":
+                points[count] += 1
+        if waterfront == "yes":
+            if state_obj.states[count]["waterfront"] == "yes":
+                points[count] += 1
+        elif waterfront == "no":
+            if state_obj.states[count]["waterfront"] == "no":
+                points[count] += 1
+        if home == "yes":
+            if state_obj.states[count]["home_price"] == "yes":
+                points[count] += 1
+        elif home == "no":
+            if state_obj.states[count]["home_price"] == "no":
+                points[count] += 1
+        if crime == "yes":
+            if state_obj.states[count]["crime_rate"] == "yes":
+                points[count] += 1
+        elif crime == "no":
+            if state_obj.states[count]["crime_rate"] == "no":
+                points[count] += 1
+
     print(points)
 
-    return flask.render_template("suggest_page.html")
+    return flask.render_template("suggest_page.html", point = points)
 
 
 @app.route("/dashboard", methods=["POST", "GET"])
